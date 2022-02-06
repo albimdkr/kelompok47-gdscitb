ba35cc772519970c48e027b9fe3b071943069238
albi mudakar <albimudakar644@gmail.com>
2022-02-07 02:07:27 +0700

    add dasbiard, login, whislist page

--- routes/pages.js
+++ routes/pages.js
@@ -11,4 +11,18 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+});
+
+router.get('/whislist', (req, res) => {
+    res.render('whislist');
+});
+
 module.exports = router;
