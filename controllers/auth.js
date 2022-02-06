5a5e69d99ebe07108f68e010703d4e485e14dbe9
albi mudakar <albimudakar644@gmail.com>
2022-02-07 00:15:32 +0700

    add module cookie parse and jsonwebtoken

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,35 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection ({
+    host : process.env.DATABASE_HOST,
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE
+});
+
+
 exports.register = (req, res) => {
     console.log(req.body);
-    res.send("form sumbitted")
+
+
+    const {name, email, password, passwordConfirm} = req.body;
+
+    db.query('SELECT email FROM users WHERE email = ?', [email], (error, results) => {
+        if (error){
+            console.log(error);
+        }
+
+        if (results.length > 0){
+            return res.render('register', {
+                message: 'Email telah siap digunakan'
+            })
+        }else if (password !== passwordConfirm){
+            return res.render('register', {
+                message: 'Password tidak cocok'
+            });
+        }
+    })
+
+    res.send("form sumbitted");
 }
+ 
