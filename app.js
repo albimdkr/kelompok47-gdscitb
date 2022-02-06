30003f1722de07e2ea20b19d98f239b8e71eb6d7
albi mudakar <albimudakar644@gmail.com>
2022-02-06 19:23:21 +0700

    add module sql, connected db-film and add .env

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require ("express");
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env'});
 
 const app = express();
+
+const db = mysql.createConnection ({
+    host : process.env.DATABASE_HOST,
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE
+});
+
+db.connect ((error) => {
+    if (error){
+        console.log(error)
+    }else {
+        console.log("Mysql connected")
+    }
+})
 
 app.get ('/', (req, res) => {
     res.send("<h1> Home page</h>")
